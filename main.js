0d8a677ac4676031c805e725ad0d708bfe6a014c
Daniel <macronetsgt@gmail.com>
2021-07-26 22:25:27 -0600

    no funciona

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data () {
+        return {
+          info: null
+        }
+      },
+      mounted () {
+        axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => (this.info = response.data))
+      }
+    })
